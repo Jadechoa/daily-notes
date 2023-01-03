e0420484f952dea2238a4757bbaae44617d0f0ab
Jade Choate <jadeschoate@gmail.com>
2023-01-03 09:43:11 -0600

    initial commit

---
+++ daily-notes/main.js
@@ -0,0 +1,34 @@
+/*
+    Define a variable named `notes` and assign a value of an empty array
+*/
+const notes = [
+    id: 1,
+    note: "Colors of a rainbow",
+    author: "Jade",
+    date: "Jul 11"
+    topics: "blue, green, yellow"
+]
+
+/*
+    Fill the array with several starter objects that have
+    the following properties.
+        * id (number)
+        * text (string)
+        * author (string)
+        * date (string)
+        * topics (array of strings)
+
+    Either define the objects within the initial array or
+    use the .push() method to add them after initialization.
+*/
+
+// create 2 more objects using .push()
+
+notes.push(
+id: 2,
+note: "days of the week",
+author: "Sharice",
+date: "July 12",
+topics: "Monday, Tuesday, Wednesday"
+)
+console.log(notes);
